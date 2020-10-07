83d07a5e2377ee488fbe36ea580bc16b7edc8aaf
Marcus Bass <mbass@Marcuss-MacBook-Pro.local>
2020-10-07 10:32:35 -0700

    pygame screen with start message

--- app_class.py
+++ app_class.py
@@ -1,5 +1,5 @@
 import sys
-import pygame 
+import pygame
 from settings import *
 
 pygame.init()
@@ -7,32 +7,49 @@ vec = pygame.math.Vector2
 
 
 class App:
+    """Application settings."""
     def __init__(self):
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) #height and width of pygame screen
         self.clock = pygame.time.Clock()
-        self.running = True
-        self.state = 'Intro'
-        
+        self.running = True #sets game to automatically run
+        self.state = 'start' #initial running state of game
     def run(self):
+        """Run Game."""
         while self.running:
-            pygame.event.get()
-            if self.state == 'intro':
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            else:
+                self.running = False
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
-    
+
+    ########## Helper Functions ################
+    def draw_text(self, words, screen, pos, size, color, font_name):
+        """Text settings."""
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, color)
+        # text_size = text.get_size()
+        screen.blit(text, pos)
+
     ########## Intro Functions ################
 
-    def intro_events(self):
+    def start_events(self):
+        """Game start functionality."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-    
-    def intro_update(self):
-        pass
+            if event.type == pygame.KEYDOWN and event.ley == pygame.K_SPACE:
+                self.state = 'playing'
 
-    def intro_draw(self):
+    def start_update(self):
+        """Game update functionality."""
+        pass
+    
+    def start_draw(self):
+        """Game start screen functionality."""
+        self.screen.fill(BLACK) 
+        self.draw_text('PUSH SPACE BAR',self.screen, [WIDTH//2, HEIGHT//2], START_TEXT_SIZE, (170, 132, 58), START_FONT)
         pygame.display.upate()
--- settings.py
+++ settings.py
@@ -1,10 +1,18 @@
 #screen settings
-WIDTH, HEIGHT = 500,700
+"""Setting the screen size."""
+WIDTH, HEIGHT = 448,596
 FPS = 60
 
 #color settings
+"""Color settings."""
+BLACK = (0,0,0)
+
+#font settings
+"""Font settings."""
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
 
 #player settings
+"""Player settings."""
 
 #mob settings
-
